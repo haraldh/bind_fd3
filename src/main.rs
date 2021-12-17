a087365071cbb1cc3a4adfeb6c1eefa3dbfef98d
Harald Hoyer <harald@profian.com>
2021-12-17 17:33:28 +0100

    initial import
    Signed-off-by: Harald Hoyer <harald@profian.com>

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![feature(io_safety)]
+
+use libc::{close, dup2, getpid};
+use std::env;
+use std::net::TcpListener;
+use std::os::unix::io::AsRawFd;
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let fd = listener.as_raw_fd();
+
+    let mut args = std::env::args().into_iter().skip(1);
+    let exe = args.next().unwrap();
+
+    let _status = unsafe {
+        Command::new(exe).args(args).pre_exec({
+            move || {
+                dup2(fd, 5);
+                dup2(5, 3);
+                close(5);
+                Ok(())
+            }
+        })
+    }
+    .status();
+
+    Ok(())
+}
